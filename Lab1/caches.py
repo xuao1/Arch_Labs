83ea61844e61a2c949027a64da4f1f07f2f420ed
xxa_local <xuao123@mail.ustc.edu.cn>
2023-04-02 19:31:23 +0800

    finish task3: add cache

---
+++ Lab1/caches.py
@@ -0,0 +1,50 @@
+from m5.objects import Cache
+
+# 创建 L1 Cache
+class L1Cache(Cache):
+    assoc = 2               # 2路组相联
+    tag_latency = 2         # 访问缓存行标记所需的时钟周期数
+    data_latency = 2        # 访问缓存行数据所需的时钟周期数
+    response_latency = 2    # 从缓存返回响应所需的时钟周期数
+    mshrs = 4               # MSHR 用于跟踪当前不在缓存中的数据的未完成请求
+    tgts_per_mshr = 20      # 每个 MSHR 中可以存储的目标数
+
+    # connectCPU 将 CPU 连接到缓存; connectBus 将缓存连接到总线
+    def connectCPU(self, cpu):
+        raise NotImplementedError
+
+    def connectBus(self, bus):
+        self.mem_side = bus.cpu_side_ports
+
+
+# 添加两个 L1Cache 的子类
+class L1ICache(L1Cache):
+    size = '16kB'
+
+    def connectCPU(self, cpu):
+        self.cpu_side = cpu.icache_port
+
+class L1DCache(L1Cache):
+    size = '64kB'
+
+    def connectCPU(self, cpu):
+        self.cpu_side = cpu.dcache_port
+
+
+# 创建 L2 Cache
+class L2Cache(Cache):
+    size = '256kB'
+    assoc = 8
+    tag_latency = 20
+    data_latency = 20
+    response_latency = 20
+    mshrs = 20
+    tgts_per_mshr = 12
+
+    def connectCPUSideBus(self, bus):
+        self.cpu_side = bus.mem_side_ports
+        # 相对于 L1 和 L2 之间的 BUS, L2 端就是 mem
+
+    def connectMemSideBus(self, bus):
+        self.mem_side = bus.cpu_side_ports
+
---
+++ Lab1/two_level.py
@@ -0,0 +1,83 @@
+import m5
+from m5.objects import *
+from caches import *
+
+# 创建系统
+system = System()
+
+# 设置时钟频率，指定电压域
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+# 内存模拟，512MB
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('512MB')]
+
+# 创建 CPU
+system.cpu = X86TimingSimpleCPU()
+
+# 创建 L1 Cache
+system.cpu.icache = L1ICache()
+system.cpu.dcache = L1DCache()
+
+# 将 Cache 连接到 CPU 端口
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+# 创建一个 L2 总线，将 L1 Cache 连接到 L2 Cache
+system.l2bus = L2XBar()
+
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+# 创建内存总线
+system.membus = SystemXBar()
+
+# 创建 L2 Cache 并将其连接到 L2 总线和内存总线
+system.l2cache = L2Cache()
+system.l2cache.connectCPUSideBus(system.l2bus)
+system.l2cache.connectMemSideBus(system.membus)
+
+# 在 CPU 上创建一个 I/O 控制器并将其连接到内存总线
+# 将系统中的一个特殊端口连接到 membus，允许系统读写内存。
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+system.system_port = system.membus.cpu_side_ports
+
+# 创建一个内存控制器并将其连接到内存总线，DDR3 控制器
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+# 完成创建模拟系统
+# 接下来，设置 CPU 要执行的进程
+
+# 创建进程
+binary = 'tests/test-progs/hello/bin/x86/linux/hello'
+
+# 设置 CPU 使用该进程作为工作负载，并在 CPU 中创建功能执行上下文
+system.workload = SEWorkload.init_compatible(binary)
+
+process = Process()
+process.cmd = [binary]
+system.cpu.workload = process
+system.cpu.createThreads()
+
+# 实例化系统并开始执行。
+root = Root(full_system = False, system = system)
+m5.instantiate()
+
+print("Beginning simulation!")
+exit_event = m5.simulate()
+
+# 输出模拟器结束运行的信息
+print('Exiting @ tick {} because {}'
+      .format(m5.curTick(), exit_event.getCause()))
+# m5.curTick() 获取当前模拟的时钟周期
+# exit_event.getCause() 获取模拟器退出的原因
+
