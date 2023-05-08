24645c2ed6d311259c0c3478d92dff12a15f2c15
xxa_local <xuao123@mail.ustc.edu.cn>
2023-05-08 21:41:46 +0800

    modify to NMRU

--- Lab3/nmru_rp.cc
+++ Lab3/nmru_rp.cc
@@ -1,35 +1,8 @@
-/**
- * Copyright (c) 2018-2020 Inria
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met: redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer;
- * redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution;
- * neither the name of the copyright holders nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "mem/cache/replacement_policies/nmru_rp.hh"
 
 #include <cassert>
 #include <memory>
+#include <vector>
 
 #include "params/NMRURP.hh"
 #include "sim/cur_tick.hh"
@@ -76,15 +49,31 @@ NMRU::getVictim(const ReplacementCandidates& candidates) const
     // There must be at least one replacement candidate
     assert(candidates.size() > 0);
 
-    // Visit all candidates to find victim
-    ReplaceableEntry* victim = candidates[0];
+    // Find the candidate with the biggest lastTouchTick
+    auto max_lastTouchTick = std::static_pointer_cast<NMRUReplData>(candidates[0]->replacementData)->lastTouchTick;
+    size_t max_index = 0;
+    for (size_t i = 1; i < candidates.size(); ++i) {
+        auto candidate_lastTouchTick = std::static_pointer_cast<NMRUReplData>(candidates[i]->replacementData)->lastTouchTick;
+        if (candidate_lastTouchTick > max_lastTouchTick) {
+            max_lastTouchTick = candidate_lastTouchTick;
+            max_index = i;
+        }
+    }
+
+    // Remove the choosen candidate
+    std::vector<ReplaceableEntry*> new_candidates(candidates.begin(), candidates.end());
+    new_candidates.erase(remaining_candidates.begin() + max_index);
+
+    //Select a random entry from the remaining candidates
+    ReplaceableEntry* victim = new_candidates[random_mt.random<unsigned>(0, new_candidates.size() - 1)];
+
+    // Visit all candidates to search for an invalid entry. If one is found,
+    // its eviction is prioritized
     for (const auto& candidate : candidates) {
-        // Update victim entry if necessary
-        if (std::static_pointer_cast<NMRUReplData>(
-                    candidate->replacementData)->lastTouchTick <
-                std::static_pointer_cast<NMRUReplData>(
-                    victim->replacementData)->lastTouchTick) {
+        if (!std::static_pointer_cast<RandomReplData>(
+                    candidate->replacementData)->valid) {
             victim = candidate;
+            break;
         }
     }
 
