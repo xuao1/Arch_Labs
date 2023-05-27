7de75c9f1ecaf072a7f32a3c1f7b79f4d93ffc9c
xxa_local <xuao123@mail.ustc.edu.cn>
2023-05-27 20:34:48 +0800

    update

---
+++ Lab5/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "7" | ./CPU0
+echo "8" | ./CPU0
+echo "9" | ./CPU0
+echo "10" | ./CPU0
+echo "11" | ./CPU0
+
+echo "7" | ./CPU1
+echo "8" | ./CPU1
+echo "9" | ./CPU1
+echo "10" | ./CPU1
+echo "11" | ./CPU1
+
+echo "7" | ./CPU2
+echo "8" | ./CPU2
+echo "9" | ./CPU2
+echo "10" | ./CPU2
+echo "11" | ./CPU2
+
+echo "7" | ./CPU2.1
+echo "8" | ./CPU2.1
+echo "9" | ./CPU2.1
+echo "10" | ./CPU2.1
+echo "11" | ./CPU2.1
