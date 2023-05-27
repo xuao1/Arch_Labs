46a39302711d6de09758fa3b4aeb281f72df31ec
xuao1 <xuao123@mail.ustc.edu.cn>
2023-05-27 06:41:04 -0700

    update

--- Lab5/run2.sh
+++ Lab5/run2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Running CPU2_1"
-echo "10 8" | ./CPU2_2
-echo "10 16" | ./CPU2_2
-echo "10 32" | ./CPU2_2
-echo "10 64" | ./CPU2_2
-echo "10 128" | ./CPU2_2
-echo "10 256" | ./CPU2_2
-echo "10 512" | ./CPU2_2
-echo "10 1024" | ./CPU2_2
+echo "11 8" | ./CPU2_2
+echo "11 16" | ./CPU2_2
+echo "11 32" | ./CPU2_2
+echo "11 64" | ./CPU2_2
+echo "11 128" | ./CPU2_2
+echo "11 256" | ./CPU2_2
+echo "11 512" | ./CPU2_2
+echo "11 1024" | ./CPU2_2
 
 
